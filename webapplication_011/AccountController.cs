77510b2d21e6b6ed64e8691ecd11cf0d9b3eb811
Sandeepk2907 <sandeepkharwar278@gmail.com>
2026-02-20 15:48:20 +0530

    Add files via upload

---
+++ webapplication_011/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebApplication_11.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> login(string username, string password)
+        {
+            if (username == "admin" && password == "123")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+                var identity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Invalid Username or Password.";
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("login");
+        }
+    }
+}
+
