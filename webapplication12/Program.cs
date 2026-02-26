1e8aadbccb55ca85e7e814e0b13241df3dd5d86e
Sandeepk2907 <sandeepkharwar278@gmail.com>
2026-02-26 11:06:39 +0530

    Add files via upload

---
+++ webapplication12/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace WebApplication12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                    options.AccessDeniedPath = "/Account/AccessDenied";
+                });
+            builder.Services.AddAuthorization();
+
+            var app = builder.Build();
+
+            
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
