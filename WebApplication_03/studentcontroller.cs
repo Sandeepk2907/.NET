cb691987259a0a01e24190d92f2315081b5a0652
Sandeepk2907 <sandeepkharwar278@gmail.com>
2026-03-05 10:41:52 +0530

    Add files via upload

---
+++ WebApplication_03/studentcontroller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace routing.Controllers
+/*{
+    public class studentcontroller : Controller
+    {
+        [Route ("Details/{id}")]
+
+        public IActionResult Index(int id)
+        {
+            return Content("Student ID is:"+ id);
+        }
+        [Route("Marks/{id}/{subject}")]
+        public IActionResult Marks(int id, string subject)
+        {
+            return Content($"Student ID is: {id} Subjects are:{subject}");
+        }
+    }
+}*/
+{
+    [Route("Student")]
+    public class studentcontroller : Controller
+    {
+        [Route("Input/{id}")]
+
+        public IActionResult Input(int id, string name)
+        {
+            string message = $"Student ID is: {id} Name are:{name}";
+            return Content(message);
+        }
+    }
+}
